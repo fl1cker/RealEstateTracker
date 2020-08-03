7cb247727afecdfb8d0f4749ca1863adaca180df
Aaron <the_flicker7@hotmail.com>
2020-08-03 18:46:27 -0400

    WIP add-marker-details

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+/* Next steps
+1) Build the UI for add marker details
+2) Add the JS to read the form on submit and store it
+3) Consider additional fields to add to the UI (perhaps a key-value pair to add anything i want on the fly)
+
+
+*/
+
 const searchSubmit = document.getElementById('address-search-submit');
 const searchAddressInput = document.getElementById('address-search-input');
+const addMarkDetailsForm = document.getElementById('add-marker-details-form');
 
 let map;
 let markers = [];
-let labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhijklmnopqrstuvwxyz1234567890';
+let markerDetails = [];
+let usableLabels =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhijklmnopqrstuvwxyz1234567890';
 let labelIndex = 0;
 
+// Edit Map
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 30.261717, lng: -81.657302 },
@@ -13,6 +25,7 @@ function initMap() {
   });
 
   const storedMarkers = JSON.parse(localStorage.getItem('markers')) || [];
+  markerDetails = JSON.parse(localStorage.getItem('markerDetails')) || [];
 
   storedMarkers.forEach((marker) => {
     addMarker(marker.lat, marker.lng, marker.label);
@@ -34,56 +47,104 @@ function initApp() {
 }
 
 function addMarker(lat, lng, label = null) {
+  const markerLabel = label || getNextUsableLabel();
+  if (!markerLabel) {
+    return;
+  }
   const marker = new google.maps.Marker({
     position: { lat, lng },
     map: map,
-    label: label || labels[labelIndex++ % labels.length],
+    label: markerLabel,
   });
 
+  removeUsableLabel(marker.label);
   markers.push(marker);
 
   google.maps.event.addListener(marker, 'rightclick', (e) => {
+    const label = marker.label;
     marker.setMap(null);
-    markers = markers.filter((x) => x.label !== marker.label);
+    markers = markers.filter((x) => x.label !== label);
+    deleteMarkerDetails(label);
   });
 }
 
-function saveMapData() {
-  const storableMarkers = markers.map((marker) => {
-    return {
-      label: marker.position.label,
-      lat: marker.position.lat(),
-      lng: marker.position.lng(),
-    };
-  });
-  localStorage.setItem('markers', JSON.stringify(storableMarkers));
-}
-
-async function plotPinByAddress(e) {
+async function plotMarkerByAddress(e) {
   e.preventDefault();
   const address = searchAddressInput.value.trim();
   if (address) {
     await getCoordinatesFromAddress(address);
+    searchAddressInput.value = '';
   }
 }
 
 async function getCoordinatesFromAddress(addressString) {
   const encodedAddress = encodeURI(addressString);
-  const result = fetch(
+  fetch(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${GOOGLE_MAPS_KEY}`
   )
     .then((result) => result.json())
     .then((data) => {
-      if (data.results.length === 0) {
+      if (
+        data.results.length === 0 ||
+        !data.results[0].formatted_address
+          .toLowerCase()
+          .includes('jacksonville')
+      ) {
+        alert('no location found');
         return;
       }
+
       const { lat, lng } = data.results[0].geometry.location;
       addMarker(lat, lng);
+      moveToLocation(lat, lng);
     });
+}
+
+function moveToLocation(lat, lng) {
+  const center = new google.maps.LatLng(lat, lng);
+  map.panTo(center);
+}
+
+// Helper Functions
+function removeUsableLabel(label) {
+  usableLabels = usableLabels.replace(label, '');
+}
+
+function getNextUsableLabel() {
+  const nextLabel = usableLabels[0];
+  if (!nextLabel) {
+    alert('No more labels available.  Delete unneeded markers');
+    return null;
+  }
+
+  return nextLabel;
+}
+
+function addMarkerDetails() {}
+
+function deleteMarkerDetails(label) {
+  markerDetails = markerDetails.filter((item) => item.label !== label);
+}
+
+function getMarkerDetails(label) {
+  return markerDetails.find((x) => x.label === label);
+}
+
+function saveMapData() {
+  const storableMarkers = markers.map((marker) => {
+    return {
+      label: marker.label,
+      lat: marker.position.lat(),
+      lng: marker.position.lng(),
+    };
+  });
+  localStorage.setItem('markers', JSON.stringify(storableMarkers));
+  localStorage.setItem('markerDetails', JSON.stringify(markerDetails));
 }
 
 // Event Listeners
 window.addEventListener('unload', saveMapData);
-searchSubmit.addEventListener('submit', plotPinByAddress);
+searchSubmit.addEventListener('submit', plotMarkerByAddress);
+addPinForm.addEventListener('submit', addMarkerDetails);
 
 initApp();
